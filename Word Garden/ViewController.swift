8feec2fd5bf6adfad1c5cd9b20f88c527ab9fdc5
Emma Loughlin <loughlem@bc.edu>
2018-09-16 13:30:06 -0400

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Emma Loughlin on 9/16/18.
+//  Copyright © 2018 Emma Loughlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
